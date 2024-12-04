55c61b54d55273792d404b754456f6d71d78f552
Gleb <glebpetrof2007@gmail.com>
2024-12-04 15:34:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-a = (input('')    #ввод строки
-b = a.split(' ')    #создание списка слов
-for i in range(len(b)):    #перебор по словам
-    for k in range(len(b[i])):    #перебор по буквам
-        print(b[i][-k-1])    #переворот слова
+string = input('')    #ввод строки
+word_list = string.split(' ')    #создание списка слов
+for i in range(len(word_list)):    #перебор по словам
+    for k in range(len(word_list[i])):    #перебор по буквам
+        print(word_list[i][-k-1], end='')    #переворот слова
+    print(' ')
