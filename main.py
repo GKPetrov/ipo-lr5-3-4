c80b7b5395e3b794880f873b7933ebb0d4f71fde
Gleb <glebpetrof2007@gmail.com>
2024-12-12 12:50:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-string = input('')    #ввод строки
+string = input('Введите строку')    #ввод строки
 word_list = string.split(' ')    #создание списка слов
-for i in range(len(word_list)):    #перебор по словам
-    for k in range(len(word_list[i])):    #перебор по буквам
-        print(word_list[i][-k-1], end='')    #переворот слова
+for word in word_list:    #перебор по словам
+    for k in range(len(word)):    #перебор по буквам
+        print(word[-k-1], end='')    #переворот слова
     print(' ')
